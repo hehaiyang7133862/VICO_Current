6395eebbbaf21eb4e5f7ac6ee287ea774a6b2d91
sea <hehaiyang7133862@163.com>
2015-09-25 09:45:19 +0800

    00.000100.0.0103
    MachionAction 更新

--- codeClient/ctrls/MachionActionCtrl.xaml.cs
+++ codeClient/ctrls/MachionActionCtrl.xaml.cs
@@ -20,15 +20,6 @@ namespace nsVicoClient.ctrls
     /// </summary>
     public partial class MachionActionCtrl : UserControl
     {
-        private bool isReversal = false;
-        public bool IsReversal
-        {
-            set
-            {
-                isReversal = value;
-            }
-        }
-
         public Image[] Image_MA_Lst1;
         public Image[] Image_MA_Lst2;
 
@@ -41,6 +32,10 @@ namespace nsVicoClient.ctrls
             {
                 Image img = new Image();
                 img.Source = (BitmapImage)App.Current.TryFindResource("MA" + i.ToString().PadLeft(2, '0'));
+                img.Height = 36;
+                img.Width = 36;
+                img.Stretch = Stretch.Fill;
+
                 Image_MA_Lst1[i] = img;
             }
 
@@ -49,90 +44,76 @@ namespace nsVicoClient.ctrls
             {
                 Image img = new Image();
                 img.Source = (BitmapImage)App.Current.TryFindResource("MA" + (i + 32).ToString().PadLeft(2, '0'));
+                img.Height = 36;
+                img.Width = 36;
+                img.Stretch = Stretch.Fill;
+
                 Image_MA_Lst2[i] = img;
             }
+
+            Image_CurrrentAction_Lst1 = new List<Image>();
+            Image_CurrrentAction_Lst2 = new List<Image>();
 
             valmoWin.dv.SysPr[106].addHandle(MachionAction1);
             valmoWin.dv.SysPr[107].addHandle(MachionAction2);
         }
 
-        private int count = 0;
 
-        private bool state1 = false;
+        private List<Image> Image_CurrrentAction_Lst1;
         private void MachionAction1(objUnit obj)
         {
-            if ((state1 == true) && (state2 == true))
-            {
-                cvsMain.Children.Clear();
-                count = 0;
-                state1 = false;
-                state2 = false;
-            }
+            Image_CurrrentAction_Lst1.Clear();
 
-            if (state1 == false)
-            {
-                int temp = obj.value;
+            int temp = obj.value;
 
-                for (int i = 0; i < 32; i++)
+            for (int i = 0; i < 32; i++)
+            {
+                if (((temp >> i) & 0x01) == 1)
                 {
-                    if (((temp >> i) & 0x01) == 1)
-                    {
-                        cvsMain.Children.Add(Image_MA_Lst1[i]);
-
-                        if (isReversal == false)
-                        {
-                            Canvas.SetLeft(Image_MA_Lst1[i], count * 40 + 4);
-                        }
-                        else
-                        {
-                            Canvas.SetRight(Image_MA_Lst1[i], count * 40 + 4);
-                        }
-                    }
+                    Image_CurrrentAction_Lst1.Add(Image_MA_Lst1[i]);
                 }
-
-                state1 = true;
             }
+
+            Refush();
         }
 
-        private bool state2 = false;
+        private List<Image> Image_CurrrentAction_Lst2;
         private void MachionAction2(objUnit obj)
         {
-            if ((state1 == true) && (state2 == true))
-            {
-                cvsMain.Children.Clear();
-                count = 0;
-                state1 = false;
-                state2 = false;
-            }
+            Image_CurrrentAction_Lst2.Clear();
 
-            if (state2 == false)
-            {
-                int temp = obj.value;
+            int temp = obj.value;
 
-                for (int i = 0; i < 32; i++)
+            for (int i = 0; i < 32; i++)
+            {
+                if (((temp >> i) & 0x01) == 1)
                 {
-                    if (((temp >> i) & 0x01) == 1)
-                    {
-                        if (((temp >> i) & 0x01) == 1)
-                        {
-                            cvsMain.Children.Add(Image_MA_Lst2[i]);
+                    Image_CurrrentAction_Lst2.Add(Image_MA_Lst2[i]);
 
-                            if (isReversal == false)
-                            {
-                                Canvas.SetLeft(Image_MA_Lst2[i], count * 40 + 4);
-                            }
-                            else
-                            {
-                                Canvas.SetRight(Image_MA_Lst2[i], count * 40 + 4);
-                            }
-                        }
-                    }
                 }
-
-                state2 = true;
             }
+
+            Refush();
         }
 
+        private void Refush()
+        {
+            cvsMain.Children.Clear();
+            int count = 0;
 
+            foreach (Image img in Image_CurrrentAction_Lst1)
+            {
+                cvsMain.Children.Add(img);
+                Canvas.SetLeft(img, 4 + count * 40);
+                count++;
+            }
+
+            foreach (Image img in Image_CurrrentAction_Lst2)
+            {
+                cvsMain.Children.Add(img);
+                Canvas.SetLeft(img, 4 + count * 40);
+                count++;
+            }
+        }
     }
 }
